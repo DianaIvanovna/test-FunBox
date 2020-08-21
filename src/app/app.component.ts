ceb0a83fa5a5c0ccb78770292008b6bb85f3e5df
Diana Garbuz <GDI.1999@yandex.ru>
2020-08-21 21:18:53 +0300

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,8 @@ export class AppComponent {
     apptitle: 'Сказочное заморское яство',
     title: 'Нямушка',
     downtitle: 'с фуа-гра' ,
-    description: '10 порций мышь в подарок',
+    description: `10 порций
+    мышь в подарок`,
     img: '',
     weight: '0,5',
     condition: 'Available', // корм есть в наличие
@@ -19,7 +20,8 @@ export class AppComponent {
     apptitle: 'Котэ не одобряет?',
     title: 'Нямушка',
     downtitle: 'с рыбой' ,
-    description: '40 порций 2 мыши в подарок',
+    description: `40 порций
+    2 мыши в подарок`,
     img: '',
     weight: '2',
     footer: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
@@ -29,7 +31,9 @@ export class AppComponent {
     apptitle: 'Сказочное заморское яство',
     title: 'Нямушка',
     downtitle: 'с курой' ,
-    description: '100 порций 5 мышей в подарок заказчик доволен',
+    description: `100 порций
+    5 мышей в подарок
+    заказчик доволен`,
     img: '',
     weight: '5',
     footer: 'Печалька, с курой закончился.',
